840a13255e52205645f30b5827bfa18b02d83f76
Bianca Karitas jensen <5884546@rts-365.dk>
2020-04-14 12:54:16 +0200

    FUCK Dropdown i template

--- dist/assets/js/dropdown.js
+++ dist/assets/js/dropdown.js
@@ -1,18 +1,31 @@
 "use strict";
 
-function myFunction(elem) {
-  var x = document.getElementById("myDropdown");
-  var description = document.getElementsByClassName("article");
-  x.innerHTML = description;
-  var button = document.getElementsByClassName("dropbtn");
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+} // Close the dropdown if the user clicks outside of it
 
-  if (elem.classList.contains("show")) {
-    elem.classList.remove("show");
 
-    for (var i = 0; i < button.length; i++) {}
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    console.log(e);
+    var myDropdown = document.getElementById("myDropdown");
 
-    button[i].classList.remove('show');
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
   }
-
-  elem.classList.add('show');
-}
+};
+/* function myFunction(elem) {
+	var x = document.getElementById("myDropdown");
+	var description = document.getElementsByClassName("article");
+	x.innerHTML = description;
+	
+	var button = document.getElementsByClassName("dropbtn");
+	if(elem.classList.contains("show")){
+		elem.classList.remove("show");
+		for (var i = 0; i < button.length; i++) {
+	}
+			button[i].classList.remove('show');
+		}
+	elem.classList.add('show');
+} */
